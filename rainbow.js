3d59ef19a6321e10d7a81a255e839dab9006b92d
Obinna Armstrong Obinna <creystyles@gmail.com>
2026-04-18 09:55:37 +0100

    Add Jumia Shop Page & Fix Site Issues

--- rainbow.js
+++ rainbow.js
@@ -1,23 +1,24 @@
-
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
-
-
+if (hamburger && navLinks) {
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
 
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
 
-// SWITCH TO SIGN UP
-signUpButton.addEventListener("click", () => {
+if (signUpButton && container) {
+  signUpButton.addEventListener("click", () => {
     container.classList.add("right-panel-active");
-});
+  });
+}
 
-// SWITCH TO SIGN IN
-signInButton.addEventListener("click", () => {
+if (signInButton && container) {
+  signInButton.addEventListener("click", () => {
     container.classList.remove("right-panel-active");
-});
+  });
+}
